70a4422d40da669b643f8a7879bd10c2575fc026
CofiShachou <filipsavic230@gmail.com>
2026-03-22 20:29:28 +0100

    filter sort add palltes add blocks to pallets ......

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,215 +1,436 @@
-debugger;
+// debugger;
 $("body").prepend(`
     <header>
         <ul>
-            <li>Nesto</li>
-            <li>Kae begi jos nesto</li>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="palletMaker.html">Pallet maker</a></li>
+            <li><a href="gallery.html">Gallery</a></li>
+            <li><a href="author.html">Author</a></li>
         </ul>
     </header>
 `)
 
-let blocks
-var con
-if(window.XMLHttpRequest){
-    con=new XMLHttpRequest()
-}
-else{
-    con= new ActiveXObject("Microsoft.XMLHYYP")
-}
-con.onreadystatechange=function(){
-    if(con.readyState==4 && con.status==200){
-        blocks=JSON.parse(this.responseText)
+let aktvniBrojStrane=1
+
+//⁢⁣⁣///////////////////////    AJAX   ///////////////////////⁡
+if(window.location.pathname=="/palletMaker.html"){
+    let blocks
+    let blocksStatic
+    var con
+    if(window.XMLHttpRequest){
+        con=new XMLHttpRequest()
     }
     else{
-        // console.log(con.status);
+        con= new ActiveXObject("Microsoft.XMLHYYP")
     }
-
-}
-con.open("GET","resources/json/blocks.json",false)
-con.send();
-
-
-///////////////    ⁡⁢⁣⁣AUTO FILL⁡   //////////////
-let colors=["Izaberite boju"]
-let categories=[]
-for(block of blocks){
-    $("#blocks").append(`
-
-        <option>`+
-        block.name
-        +`</option>
-    `)
-}
-let ima=false
-for(block of blocks){
-    $("#color option").each(function(){
-        if(block.color==$(this).val()){
-            ima=true
+    con.onreadystatechange=function(){
+        if(con.readyState==4 && con.status==200){
+            blocks=JSON.parse(this.responseText)
+            blocksStatic=JSON.parse(this.responseText)
         }
-    })
-    if(ima==false){
-        colors.push(block.color)
-        $("#color").append(`
-    
-            <option>`+
-            block.color
-            +`</option>
-        `)
+        else{
+            // console.log(con.status);
+        }
+
     }
-    ima=false
-}
-let appTex
-function appSve(){
+    con.open("GET","resources/json/blocks.json",false)
+    con.send();
+//⁡⁣⁣⁢/////////////////////////////////////////////////////////////////////⁡
+
+//⁡⁢⁣⁣/////////////////    AUTO FILL   ////////////////////⁡
+
+
+    let colors=["Chose color"]
+    let categories=[]
+    let checkedCategories=[]
+    let chosenColor
+    let ima=false
+    let blocksFiltered
+    let blocksFilteredName
+    let blocksFilteredCategory
+    let blocksFilteredColor
+    let searchText
+    let filterAll
+    let izabraniBlock
+
     
-    $(".catalog").text("")
+
     for(block of blocks){
-        // console.log("Ubaci");
-        Append()
-    }  
-}
-appSve()
-
-let appCategeory
-let hasCategory=false
-for(block of blocks){
-    $("#category :checkbox").each(function(){
-        if(block.category==$(this).val()){
-            hasCategory=true
+        $("#color option").each(function(){
+            if(block.color==$(this).val()){
+                ima=true
+            }
+        })
+        if(ima==false){
+            colors.push(block.color)
+            $("#color").append(`
+        
+                <option>`+
+                block.color
+                +`</option>
+            `)
         }
-    })
-    if(hasCategory==false){
-        categories.push(block.category)
-        $("#category").append(`
-            <div>
-                <input type="checkbox" value="`+block.category+`" id="`+block.category+`""></input>
-                <label for="`+block.category+`">`+block.category+`</label>
-            <div>
+        ima=false
+    }
+
+    
+    
+
+    let appTex
+
+    function appSve(){
+
+        // $(".page").text("")
+        $(".page").empty()
+
+        if($("#commingSoon h2").text()){
+            $("#commingSoon").remove()
+        }
+        let br=1;
+
+        
+        // console.log("DSADHJASFGDHASYUSDGHJKAS= "+JSON.stringify(blocksFilteredColor));
+        // console.log("Name filtered= "+JSON.stringify(blocks));
+        if($("#color").val()=="Chose color" && checkedCategories.length==0 && $("#name").val()==""){
+            blocks=blocksStatic
+        }
+        else{
+            blocks=filterAll
+        }
+        // if($("#color").val()!="Chose color" && checkedCategories.length==0){
+        //     blocks=blocksFilteredColor
+        // }
+        // if($("#color").val()=="Chose color" && checkedCategories.length!=0){
+        //     blocks=blocksFilteredCategory
+        // }
+        // if($("#color").val()!="Chose color" && checkedCategories.length!=0){
+        // }
+
+        // console.log("DSADHJASFGDHASYUSDGHJKAS= "+JSON.stringify(blocks));
+
+
+        let brojStrana=(blocks.length /12)
+        brojStrana=Math.ceil(brojStrana)
+
+
+
+
+        $(".pages").empty()
+        for(let i=0;i<brojStrana;i++){
+            $(".pages").append(`<p>`+(i+1)+`</p>`)
+        }
+
+
+        // console.log("BLOKS= "+JSON.stringify(blocks));
+        
+        if($("#sort").val()=="Name asc"){
+            blocks=blocks.sort((a,b)=>a.name.localeCompare(b.name))
+        }
+        if($("#sort").val()=="Name desc"){
+            blocks=blocks.sort((a,b)=>b.name.localeCompare(a.name))
+        }
+        if($("#sort").val()=="Id asc"){
+            blocks=blocks.sort((a,b)=>a.id-b.id)
+        }
+        if($("#sort").val()=="Id desc"){
+            blocks=blocks.sort((a,b)=>b.id-a.id)
+        }
+
+        for(block of blocks){
+            if(br>1*12 && br<=aktvniBrojStrane*12 && aktvniBrojStrane==2){
+                append()
+            }
+            if(br>=1 && br<=12 && aktvniBrojStrane==1){
+                append()
+            }
+            br++
+        }
+    }
+    appSve()
+
+    let appCategeory
+    let hasCategory=false
+    for(block of blocks){
+        $("#category :checkbox").each(function(){
+            if(block.category==$(this).val()){
+                hasCategory=true
+            }
+        })
+        if(hasCategory==false){
+            categories.push(block.category)
+            $("#category").append(`
+                <fieldset name="chategory">
+                    <input type="checkbox" value="`+block.category+`" id="`+block.category+`""></input>
+                    <label for="`+block.category+`">`+block.category+`</label>
+                </fieldset>
+            `)
+        }
+        hasCategory=false
+    }
+
+    let sortCrits=["Id asc","Id desc","Name asc","Name desc"]
+
+    for(sort of sortCrits){
+        $("#sort").append(`
+            <option id="`+sort+`" value="`+sort+`">`+sort+`</option>
         `)
     }
-    hasCategory=false
-}
 
-////////////    ⁡⁢⁣⁣USER INPUT⁡⁡   //////////////
-$("#blocks").change(()=>{
-    console.log($("#blocks").val());
     
-})
-$("#color").change(function (){
-    
-    SaveFilters($(this).val(),null)
-    // ShowItems()
-})
+//⁡⁣⁣⁢///////////////////////////////////////////////////////////////////////⁡
 
-$("#category :checkbox").change(function(){
-    SaveFilters($(this).val(),$(this).is(":checked"))
-    
-    // JSON.parse(localStorage.getItem("filter"))[0].color
-    // console.log("SAIODHJASG= "+$(this).is(":checked"));
-    // ShowItems()
-    
-})
+//⁡⁢⁣⁣/////////////////////    USER INPUT   ////////////////////⁡
+    $("#color").change(function (){
+        chosenColor=$("#color").val()
+        filtered()
+    })
+    $("#name").on("input",()=>{
+        // console.log($("#name").val());
+        searchText=$("#name").val()
+        filtered()
+    })
 
-
-////////////  ⁡⁢⁣⁣FUNCTIONS⁡   ////////////////////////////
-function Append(){
-    appText=`<div class="item">`
-    appText+=`
-    <img src="resources/images/`+block.img+`"/>
-    <h2>`+block.name+`</h2>
-    <p>Color: `+block.color+`</p>
-    <p>Category: `+block.category+`</p>
-    `
-    appText+=`</div>`
-    $(".catalog").append(appText)
-    // console.log("TRENUTNI BLOCK= "+JSON.stringify(block));
-    
-}
-
-function ShowItems(){
-    $(".catalog").text("")
-    let x=JSON.parse(localStorage.getItem("filter"))
-    console.log("X= "+JSON.stringify(x));
-    
-    let dodati=[]
-    let itemPostojiToggle=false
-    if(x[0].color!="Izaberite boju" || x[1][0].category!="sve"){
-        for(block of blocks){
-            for(cat of x[1]){
-                if(block.category==cat.category){
-                    for(item of dodati){
-                        if(block.name==item) itemPostojiToggle=true
-                    }
-                    if(!itemPostojiToggle){
-                        Append()
-                        dodati.push(block.name)
-                    }
-                    itemPostojiToggle=false
-                    console.log("DODATI= "+dodati);
-                }
-            }
-            if(block.color==x[0].color ){
-                for(item of dodati){
-                    if(block.name==item) itemPostojiToggle=true
-                }
-                if(!itemPostojiToggle){
-                    Append()
-                    dodati.push(block.name)
-                }
-                itemPostojiToggle=false
-            }
+    $("#category :checkbox").change(function(){
+        aktvniBrojStrane=1
+        if($(this).is(":checked")){
+            checkedCategories.push($(this).val())
         }
-    }
-    else{
+        else{
+            checkedCategories=checkedCategories.filter(x=>x != $(this).val())
+        }
+        
+        filtered()
+    })
+    $(document).on("click",".pages p",function(){
+        let strane=$(this).text();
+        aktvniBrojStrane=Number(strane)
+        ShowItems()
+    })
+
+    $("#sort").change(()=>{
         appSve()
-        dodati=[]
-        console.log("SVE");
+    })
 
-    }
-}
-let filter
+    
+//⁡⁣⁣⁢/////////////////////////////////////////////////////////////////////⁡
 
-
-// ⁡⁢⁣⁣LOCAL STORAGE⁡ //
-filter=
-[
-    {"color":"Izaberite boju"},
-    [
-        {"category":"sve"}
-    ]
-]
-function SaveFilters(value,ck){
-    for(color of colors){
-        if(value==color){
-            filter[0].color=color
-        }
-    }
-    for(category of categories){
-        if(value==category){
+//⁡⁢⁣⁣//////////////////////  FUNCTIONS   ////////////////////////////⁡
+    function append(){
+        appText=`<div class="item">`
+        appText+=`
+        <img src="resources/images/`+block.img+`"/>
+        <h2>`+block.name+`</h2>
+        <p>Color: `+block.color+`</p>
+        <p>Category: `+block.category+`</p>
+        <p class="invisibleId">`+block.id+`</p>
+        <button class="addBlock">Add block</button>
+        `
+        appText+=`</div>`
+        $(".page").append(appText)  
             
-            if(ck){
-                filter[1]=filter[1].filter(v=>v.category!=="sve")
-                filter[1].push({"category":category})  
-            // console.log(filter[1]);
+    }
 
-            }
-            else{
-                if(filter[1].length==1){
-                    filter[1]=filter[1].filter(v=>v.category!==value)
-                    filter[1].push({"category":"sve"})       
+
+    function filtered(){
+
+        blocksFilteredCategory = blocksStatic.filter(x => checkedCategories.includes(x.category));
+        blocksFilteredColor = blocksStatic.filter(x => x.color==chosenColor);
+        blocksFilteredName=blocksStatic.filter(x=>x.name.includes(searchText))
+
+        // console.log("Name= "+searchText);
+        
+        filterAll=[...new Set([...blocksFilteredCategory,...blocksFilteredColor,...blocksFilteredName])]
+        console.log("Filter all= "+JSON.stringify(filterAll));
+
+        // console.log("FILTERED COLOR= "+JSON.stringify(blocksFilteredColor));
+        
+
+        // blocksFiltered=blocksStatic.filter(x=>)
+        // console.log(blocksFiltered);
+        appSve()
+    }
+    function ShowItems(){
+        $(".page").text("")
+
+        $(".page").text("")
+        if($("#commingSoon h2").text()){
+            $("#commingSoon").remove()
+        }
+        let br=1;
+        appSve()
+    }
+
+
+
+//⁡⁣⁣⁢/////////////////////////////////////////////////////////////⁡
+
+
+
+
+
+//⁡⁢⁣⁣/////////////////////////////////   PALLETS /////////////////////////////////⁡
+
+
+
+
+
+$(document).on("click",".addBlock",function(){
+    izabraniBlock=$(this).siblings(".invisibleId").text()
+
+    $("#popUp").css({
+        "display":"flex"
+    })
+
+    setTimeout(() => {
+        $("#popUp").css({
+            "opacity":"1"
+        })
+    }, 100);
+
+    let pallets=JSON.parse(localStorage.getItem("pallets")) || []
+    $("#pallets").empty()
+
+    let appText2=""
+    for(pallet of pallets){
+        appText2=`<div id="`+pallet.id+`">
+            <p>`+pallet.name+`</p>
+            <div class="miniPallets">`
+
+
+
+        if(pallet.blocks.length){
+            for(block of blocks){
+                for(pal of pallet.blocks){
+                    if(block.id == pal)
+                        appText2+=`<img src=resources/images/`+block.img+` alt="block">`
                 }
-                else{
-                    filter[1]=filter[1].filter(v=>v.category!==value)
+            }
+            for(let i=6-pallet.blocks.length;i>0;i--){
+                appText2+=`<img src=resources/images/default.png alt="block">`
+            }
+        }
+        else{
+            for(let i=0;i<6;i++){
+                appText2+=`<img src="resources/images/default.png" alt="block">`
+            }     
+        }
+        
+        appText2+=`</div></div>`
+        $("#pallets").append(appText2)
+    }
+    
+    
+})
+$("#closePallets").click(()=>{
+    $("#popUp").css({
+        "opacity":"0"
+    })
 
-                    console.log("ne");
+    setTimeout(() => {
+        $("#popUp").css({
+            "display":"none"
+        })
+    }, 200);
+})
 
+$("#addPallet").click(()=>{
+    setTimeout(() => {
+        $("#createPallet").css({
+            opacity:1,
+            height:"2.5vw",
+        })
+    }, 100);
+})
+
+$(document).on("click","#cancle",function(){
+    setTimeout(() => {
+            $("#createPallet").css({
+                opacity:0,
+                height:"0px",
+            })
+        }, 100);
+})
+$(document).on("click","#create",function(){
+    let brojObjekata=JSON.parse(localStorage.getItem("pallets"))
+    brojObjekata=brojObjekata.length
+    console.log("Broj objekata= "+brojObjekata);
+    
+    if(brojObjekata<6){
+
+        let palletName=$("#palletName").val()
+
+        $("#palletName").val("")
+        let palletId=0
+        let numberOfPallets=localStorage.getItem("numberOfPallets")
+        
+        palletId=++numberOfPallets
+        localStorage.setItem("numberOfPallets",numberOfPallets)
+        // localStorage.setItem("numberOfPallets",JSON.stringify(0))
+        
+        
+        let pallet={
+            id:palletId,
+            name:palletName,
+            blocks:[]
+        }
+        let pallets=JSON.parse(localStorage.getItem("pallets")) || []
+        // let pallets
+        pallets.push(pallet)
+        localStorage.setItem("pallets",JSON.stringify(pallets))
+        
+        setTimeout(() => {
+            $("#createPallet").css({
+                opacity:0,
+                height:"0px",
+            })
+        }, 100);
+        $("#pallets").append(`
+            <div id="`+palletId+`">
+            <p>`+palletName+`</p>
+            <div class="miniPallets">
+                <img src="resources/images/default.png" alt="block">
+                <img src="resources/images/default.png" alt="block">
+                <img src="resources/images/default.png" alt="block">
+                <img src="resources/images/default.png" alt="block">
+                <img src="resources/images/default.png" alt="block">
+                <img src="resources/images/default.png" alt="block">
+            </div>
+            </div>
+            `)
+        }
+        else{
+            console.log("Nije moguce imati vise od 6 paleta");
+            
+        }
+})
+
+
+$(document).on("click","#pallets>div",function(){
+    // console.log($(this).children(".miniPallets").children("img").attr("src"));
+    let izabranaPaleta=$(this).attr("id")
+    for(let i=1;i<=6;i++){
+        if($(this).children(".miniPallets").children("img:nth-child("+i+")").attr("src")=="resources/images/default.png"){
+            for(block of blocks){
+                if(block.id == izabraniBlock){
+                    $(this).children(".miniPallets").children("img:nth-child("+i+")").attr("src","resources/images/"+block.img+"")
+                    break
                 }
             }
-
+            break
         }
     }
-    // console.log("FILTER=  "+JSON.stringify(filter));
-    localStorage.setItem("filter",JSON.stringify(filter))
-    ShowItems()
-}
+    console.log("KRAJ");
+    let updatePallets=JSON.parse(localStorage.getItem("pallets"))
 
-SaveFilters()
+    let palletToChange=updatePallets.find(x=>x.id==izabranaPaleta)
+    palletToChange.blocks.push(izabraniBlock)
+
+    localStorage.setItem("pallets",JSON.stringify(updatePallets))
+    // localStorage.setItem("pallets",updatePallets)
+    
+})
+
+
+
+}
+//⁡⁣⁣⁢///////////////////////////////////////////////////////////////⁡
